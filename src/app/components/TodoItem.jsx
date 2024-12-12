c179fe9f0ad8dba53624e4e06cdcac5e52719647
MichaelPfen <knxcksopa@gmail.com>
2024-12-12 15:29:38 +0100

    priorisierung von todo items

--- src/app/components/TodoItem.jsx
+++ src/app/components/TodoItem.jsx
@@ -41,35 +41,55 @@ const TodoItem = (props) => {
     []
   );
 
+  const priorityStyle = {
+    hoch: { color: "red", fontWeight: "bold" },
+    mittel: { color: "orange" },
+    niedrig: { color: "green" },
+  };
+
   return (
-    <li className={styles.item} data-type="todo-item">
-      <div onDoubleClick={handleEditing} style={viewMode}>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => props.handleChangeProps(id)}
-          name="checkbox"
-        />
-        <button
-          data-set="delete-todo-btn"
-          onClick={() => props.deleteTodoProps(id)}
-        >
-          <FaTrash style={{ color: "orangered", fontSize: "16px" }} />
-        </button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </div>
-      <input
-        type="text"
-        style={editMode}
-        className={styles.textInput}
-        value={title}
-        onChange={(e) => {
-          props.setUpdate(e.target.value, id);
-        }}
-        onKeyDown={handleUpdatedDone}
-      />
-    </li>
+      <li className={styles.item} data-type="todo-item">
+          <div onDoubleClick={handleEditing} style={viewMode}>
+              <input
+                  type="checkbox"
+                  className={styles.checkbox}
+                  checked={completed}
+                  onChange={() => props.handleChangeProps(id)}
+                  name="checkbox"
+              />
+              <span style={priorityStyle[props.todo.priority]}>
+        [{props.todo.priority}]
+      </span>
+              <span style={completed ? completedStyle : null}>{title}</span>
+              <button
+                  data-set="delete-todo-btn"
+                  onClick={() => props.deleteTodoProps(id)}
+              >
+                  <FaTrash style={{color: "orangered", fontSize: "16px"}}/>
+              </button>
+          </div>
+          <input
+              type="text"
+              style={editMode}
+              className={styles.textInput}
+              value={title}
+              onChange={(e) => {
+                  props.setUpdate('title', e.target.value, id);
+              }}
+              onKeyDown={handleUpdatedDone}
+          />
+          <select
+              value={props.todo.priority}
+              onChange={(e) => props.setUpdate('priority', e.target.value, id)} // priority ändern
+              className={styles.prioritySelect}
+          >
+              <option value="hoch">Hoch</option>
+              <option value="mittel">Mittel</option>
+              <option value="niedrig">Niedrig</option>
+          </select>
+
+      </li>
+
   );
 };
 
